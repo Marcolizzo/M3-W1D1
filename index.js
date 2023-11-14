f46eb4bee9c8295f27a5281627bc7a463583ff29
Marco Santoro <msantor@hotmail.it>
2023-11-14 21:00:38 +0100

    CSS Avanzato
    Esercizio extra 2 da sistemare

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 //Esercizio extra 1
 const cancelTwitter = function () {
     let linkTwitter = document.querySelector("main .p-4:last-of-type li:nth-of-type(2)")
-    linkTwitter.innerHTML="<style> display: none </style>"
+    linkTwitter.remove()
 }
 cancelTwitter()
+
+//Esercizio extra 2
+const linkLeggi = document.querySelector(".jumbotron a")
+const cancelFather = function () {
+    let father = document.querySelector(".jumbotron")
+    father.remove()
+}
+linkLeggi.addEventListener("click", cancelFather)
+//Esercizio extra 3
